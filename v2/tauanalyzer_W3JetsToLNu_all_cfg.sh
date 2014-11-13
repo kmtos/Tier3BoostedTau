9159079afaa428cf8f78d954022d7fb4fed76daa
kyle tos <kmtos@ucdavis.edu>
2014-11-13 09:07:00 -0800

    Removed directories

--- v2/tauanalyzer_W3JetsToLNu_all_cfg.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "Running tauanalyzer_W3JetsToLNu_all_cfg.sh"
-jobDir="/home/kmtos/NMSSM_Analysis/CMSSW_5_3_11/src/BoostedTauAnalysis/TauAnalyzer/test/v2"
-fileNamePrefix="tauanalyzer_W3JetsToLNu"
-
-eval `scramv1 runtime -sh`
-cmsRun ${fileNamePrefix}_all_cfg.py
-echo "cmsRun ${fileNamePrefix}_all_cfg.py DONE"
-exit 0
